cad48b3f30c9d7c5c0bd4329e9489011fd1c003a
wisdom3541 <ezeokoliwisdom@gmail.com>
2019-11-11 22:15:04 -0800

    //adding maps fragments// //code running but google maps isn't coming up...and no error or app crashing// //added getlocation and delete user option in listview//

--- app/src/main/java/com/example/findme/friendjava.java
+++ app/src/main/java/com/example/findme/friendjava.java
@@ -1,10 +1,11 @@
 package com.example.findme;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +17,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class friendjava extends Fragment {
+    AlertDialog.Builder alert;
 
+   ListView list;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.friends, container, false);
 
 
-        final ListView list = (ListView) view.findViewById(R.id.listview1);
+         list = (ListView) view.findViewById(R.id.listview1);
 
         final ArrayList<String> mylist = new ArrayList<>();
         mylist.add("wisdom");
@@ -39,7 +42,18 @@ public class friendjava extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Toast.makeText(getContext(),"Getting location",Toast.LENGTH_SHORT).show();
+                alert.setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("User")
+                        .setMessage("Select Options")
+                        .setPositiveButton("Get location", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                Toast.makeText(getContext(),"Logging Out", Toast.LENGTH_LONG).show();
+                                  }
+                        })
+                        .setNegativeButton("Delete Friend", null);
+                        Toast.makeText(getContext(),"Deleting User",Toast.LENGTH_LONG).show();
+
 
             }
         });
