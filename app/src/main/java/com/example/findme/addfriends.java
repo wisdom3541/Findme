75849856a4b20610392337c19ad51f8c1961035c
wisdom3541 <ezeokoliwisdom@gmail.com>
2020-02-14 00:28:15 +0100

    //working on retriving firebase data and implementing google and facebook login //settting fragment in progress //finalizing other fragments

--- app/src/main/java/com/example/findme/addfriends.java
+++ app/src/main/java/com/example/findme/addfriends.java
@@ -7,23 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class addfriends extends Fragment {
 
     EditText add;
+    Button addf;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.addfriends,container,false);
 
-        add = (EditText) view.findViewById(R.id.addf);
+        add = (EditText) view.findViewById(R.id.addftext);
+        addf = (Button) view.findViewById(R.id.addf);
 
 
         //toast
         add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               add.setText("");
+            }
+        });
+
+        addf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(),"Friend Added",Toast.LENGTH_LONG);
--- app/src/main/java/com/example/findme/settings.java
+++ app/src/main/java/com/example/findme/settings.java
@@ -7,14 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class settings extends Fragment {
+
+    Button editpro;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings, container, false);
 
+        editpro= (Button) view.findViewById(R.id.editprobtn);
+
+
+        editpro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
        return view;
     }
