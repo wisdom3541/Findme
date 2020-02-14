eb005b152e34c52b0699e998bdb5f66b46c39966
wisdom3541 <ezeokoliwisdom@gmail.com>
2020-02-14 23:55:42 +0100

    //working on retriving firebase data and implementing google and facebook login //settting fragment in progress //finalizing other fragments

--- app/src/main/java/com/example/findme/settings.java
+++ app/src/main/java/com/example/findme/settings.java
@@ -9,11 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class settings extends Fragment {
 
     Button editpro;
+    TextView email;
     Intent intent;
+    String email1;
 
     @Nullable
     @Override
@@ -21,7 +27,19 @@ public class settings extends Fragment {
         View view = inflater.inflate(R.layout.settings, container, false);
 
         editpro= (Button) view.findViewById(R.id.editprobtn);
+        email = (TextView) view.findViewById(R.id.useremail);
         intent = new Intent(getContext(),editprofile.class);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address
+            String name = user.getDisplayName();
+             email1 = user.getEmail();
+
+             email.setText(email1);
+        }
+
+
 
 
         editpro.setOnClickListener(new View.OnClickListener() {
